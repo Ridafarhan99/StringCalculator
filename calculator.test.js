2999d06152adb072a3ad99d314b35bedbea1a815
Rida Farhan <42339599+Ridafarhan99@users.noreply.github.com>
2024-11-25 02:46:05 +0530

    handle negative numbers

--- calculator.test.js
+++ calculator.test.js
@@ -23,3 +23,9 @@ test("should handle newlines as delimiters", () => {
 test("should support custom delimiters", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+
+test("should throw an exception for negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow(
+    "negative numbers not allowed: -2, -4"
+  );
+});
